5950bf18bb2ec77d97e2cf524135399b54d8bb6f
Александр Степин < >
2017-03-14 20:45:37 +0700

    ghbdtndfg

---
+++ Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Concrete
+{
+   public class EFDbContext: DbContext
+    {
+        public DbSet<item> items { get; set; }
+        public DbSet<Cart> Carts { get; set; } 
+    }
+}
---
+++ Domain/Concrete/EFItemRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Abstract;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Concrete
+{
+    public class EFItemRepository: IItemRepository
+    {
+        EFDbContext context = new EFDbContext();
+        public IEnumerable<item> items
+        {
+            get { return context.items;  }
+        }
+    }
+}
